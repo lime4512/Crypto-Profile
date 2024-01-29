defb2daeb965d021f3ab5b130cbee5fbf44d46a7
Alex Kruglov <lime4512@mail.ru>
2024-01-30 01:51:15 +0300

    add api and img coin

--- src/components/trackCoin/TrackCoin.tsx
+++ src/components/trackCoin/TrackCoin.tsx
@@ -1,36 +1,38 @@
+import { useEffect, useState } from 'react'
 import '../../style/trackCoin/trackCoin.scss'
+import axios from 'axios'
 
 export const TrackCoin = () => {
+	const [data, setData] = useState([])
+	useEffect(() => {
+		const CoinApi = async () => {
+			axios
+				.get(
+					'https://api.coinlore.net/api/ticker/?id=90,80,518,2710,48543,58,33285,46971,257,44883,2,2713,45219,2751,54683,45088'
+				)
+				.then(res => {
+					setData(res.data)
+				})
+				.catch(err => {
+					console.log(err)
+				})
+		}
+		CoinApi()
+	}, [])
+
 	return (
 		<section className='trackCoin'>
-			<h1>Coin</h1>
-			<h1>Coin</h1>
-			<h1>Coin</h1>
-			<h1>Coin</h1>
-			<h1>Coin</h1>
-			<h1>Coin</h1>
-			<h1>Coin</h1>
-			<h1>Coin</h1>
-			<h1>Coin</h1>
-			<h1>Coin</h1>
-			<h1>Coin</h1>
-			<h1>Coin</h1>
-			<h1>Coin</h1>
-			<h1>Coin</h1>
-			<h1>Coin</h1>
-			<h1>Coin</h1>
-			<h1>Coin</h1>
-			<h1>Coin</h1>
-			<h1>Coin</h1>
-			<h1>Coin</h1>
-			<h1>Coin</h1>
-			<h1>Coin</h1>
-			<h1>Coin</h1>
-			<h1>Coin</h1>
-			<h1>Coin</h1>
-			<h1>Coin</h1>
-			<h1>Coin</h1>
-			<h1>Coin</h1>
+			{data.map(item => (
+				<div key={Math.random()}>
+					<h1>{item.symbol}</h1>
+					<h1>{item.price_usd}</h1>
+					<img
+						className='img-coin'
+						src={'/imgCoin/' + item.symbol.toLowerCase() + '.png'}
+						alt=''
+					/>
+				</div>
+			))}
 		</section>
 	)
 }
